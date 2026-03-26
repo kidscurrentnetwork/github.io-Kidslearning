0bc15ee551c542425c6688ad22bed99b907045ea
kidscurrentnetwork <kidscurrentnetwork@gmail.com>
2026-03-25 21:55:48 -0400

    Add SunnySideStreetLandingPage component

---
+++ SunnySideStreetLandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SunnySideStreetLandingPage = () => {
+    return (
+        <div>
+            <h1>Welcome to Sunny Side Street</h1>
+            <p>Your journey to learning starts here!</p>
+        </div>
+    );
+};
+
+export default SunnySideStreetLandingPage;
